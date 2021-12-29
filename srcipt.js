4b66a03aa3a5ce5cddacf4057cf57c771c189469
Ngô Gia Nhất Sơn <69605874+ngns2002@users.noreply.github.com>
2021-12-29 15:42:49 +0700

    Update srcipt.js

--- srcipt.js
+++ srcipt.js
@@ -6,3 +6,10 @@ var social_input = document.getElementById('social_input');
 
 
 for (var i = 0, length = a.length; i < length; i++) { a[i].onclick=function(e) { var social_link=e.target.getAttribute('link'); social_input.value=social_link; }; } copy.addEventListener('click',function(){ social_input.select(); document.execCommand('copy'); });
+
+$(window).on( 'load', function() {
+	$(".loadding-page").delay(3000).fadeOut(200);
+	$(".cssload-box-loading").on('click', function() {
+	    $(".cssload-box-loading").fadeOut(200);
+	})
+})
